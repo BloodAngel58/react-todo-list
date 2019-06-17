9225fecf1d0967611e5b53e4387f95d7905e4693
BloodAngel58 <bloodange2013@yandex.ru>
2019-06-17 11:43:56 +0300

    update project

---
+++ src/components/form.js
@@ -0,0 +1,9 @@
+import React from "react";
+//import "./form.css";
+import Task from "./task.js";
+
+
+class Form extends React.Component {
+
+}
+export default Form;
---
+++ src/components/task.js
@@ -0,0 +1,53 @@
+import React from "react";
+//import "./task.css";
+
+class Task extends React.Component {
+
+    render() {
+        const {
+            text,
+            date,
+            isChecked,
+            id,
+            checkHandler,
+            deleteHandler
+        } = this.props;
+        return ( <
+            div className = "task"
+            id = {
+                id
+            }
+            key = {
+                id
+            } >
+            <
+            input type = "checkbox"
+            className = "toggle"
+            onChange = {
+                () => checkHandler(id)
+            }
+            checked = {
+                isChecked
+            }
+            /> <
+            label className = {
+                isChecked ? "text-field line-through" : "text-field"
+            } > {
+                text
+            } < /label> <
+            span className = "date" > {
+                date
+            } < /span> <
+            input type = "button"
+            className = "destroy"
+            value = "x"
+            onClick = {
+                () => deleteHandler(id)
+            }
+            />  < /
+            div >
+        );
+    }
+}
+
+export default Task;
