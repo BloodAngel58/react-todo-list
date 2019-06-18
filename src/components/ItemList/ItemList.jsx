e0a3331e802a13e25a43ceff371819bfcec77988
BloodAngel58 <bloodange2013@yandex.ru>
2019-06-18 11:20:02 +0300

    Cleaned up the code

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -3,17 +3,17 @@ import Item from "../Item/Item";
 class ItemList extends React.Component {
   render() {
     const { data } = this.props;
-    let newsTemplate = null;
+    let newsTask = null;
 
     if (data.length) {
-      newsTemplate = data.map(function(item) {
+      newsTask = data.map(function(item) {
         return <Item key={item.id} data={item} />;
       });
     } else {
-      newsTemplate = <p>К сожалению новостей нет</p>;
+      newsTask = <p>К сожалению новостей нет</p>;
     }
 
-    return newsTemplate;
+    return newsTask;
   }
 }
 export default ItemList;
